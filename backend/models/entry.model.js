ebee6f3486846663b892ceaa7049bb28b6492562
immanuelsiregar <61646253+immanuelsiregar@users.noreply.github.com>
2020-05-20 16:24:10 +1200

    added entries.js
    a route file for entry creation

--- backend/models/entry.model.js
+++ backend/models/entry.model.js
@@ -3,7 +3,8 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const entrySchema = new Schema({
-  entry: { type: String, required = true },
+  type: { type: String, required = true },
+  key: { type: String, required = true},
   title: { type: String, required = true },
   author: { type: String, required = true },
   journal: { type: String },

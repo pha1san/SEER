56a140a3b05b65bc6782ab7fefea3e4140b3c77c
Phaisan1233 <42394385+Phaisan1233@users.noreply.github.com>
2020-05-25 15:40:09 +1200

    update

--- frontend/src/components/edit-exercise.component.js
+++ frontend/src/components/edit-exercise.component.js
@@ -1,31 +1,36 @@
-import React, { Component } from 'react';
-import axios from 'axios';
+import React, { Component } from "react";
+import axios from "axios";
 import "react-datepicker/dist/react-datepicker.css";
 
 export default class EditExercise extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      article: ''
-    }
+      article: "",
+    };
   }
   componentDidMount() {
-    axios.get("/entries/" + this.props.match.params.id)
-      .then(response => {
-        let myString = JSON.stringify(response.data, "", "\ ").trim()
-          .replace('{', '').replace('}', '');
+    axios
+      .get("/entries/" + this.props.match.params.id)
+      .then((response) => {
+        let myString = JSON.stringify(response.data, "", " ")
+          .trim()
+          .replace("{", "")
+          .replace("}", "");
         console.log(myString);
-        this.setState({ article: myString.replace(',', '\n') })
-      }).catch(function (error) {
-        console.log(error);
+        this.setState({ article: myString.replace(",", "\n") });
       })
+      .catch(function (error) {
+        console.log(error);
+      });
   }
+
   render() {
     return (
       <div>
         <p>{this.state.article}</p>
       </div>
-    )
+    );
   }
 }
 //

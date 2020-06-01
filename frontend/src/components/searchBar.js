856299fde0da67be6d4fa850909d00dbb2b5a136
immanuelsiregar <61646253+immanuelsiregar@users.noreply.github.com>
2020-06-01 14:59:27 +1200

    Create searchBar.js

---
+++ frontend/src/components/searchBar.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react'
+
+
+export class searchBar extends Component {
+    constructor(props){
+        this.state = {
+            searchText = ""
+        }
+    }
+    
+    
+    render() {
+        return (
+            <div>
+                
+            </div>
+        )
+    }
+}

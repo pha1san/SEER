4f48376f640eac6e52968fecdf341c658ad6e2f3
Phaisan1233 <42394385+Phaisan1233@users.noreply.github.com>
2020-06-15 04:34:30 +1200

    unit testing

--- test/test.js
+++ test/test.js
@@ -4,7 +4,8 @@ const chaiHttp = require("chai-http");
 const entryModel = require("../backend/models/entryModel");
 const tdd_article = require("../backend/tdd_articles.json");
 let request = require("supertest");
-let { app, mongoose } = require("../backend/server");
+let app;
+let mongoose;
 
 chai.use(chaiHttp);
 chai.should();
@@ -17,23 +18,39 @@ describe("Array", function () {
   });
 });
 
-describe("*********** ARTICLES ***********", () => {
-  describe("/GET Request to get all articles", () => {
-    it("Should get an array of articles", (done) => {
-      chai
-        .request(app)
-        .get("/entries")
-        .end((err, res) => {
-          res.should.have.status(200);
-          res.body.should.be.a("array");
-          done();
-        });
-    }).timeout(5000);
-  });
-});
+describe("------------ API Routing ------------", () => {
+  before(() => {
+    app = require("../backend/server").app;
+    mongoose = require("../backend/server").mongoose;
+  }),
+    describe("/GET Request to get all articles", () => {
+      it("Should get an array of articles", (done) => {
+        chai
+          .request(app)
+          .get("/entries")
+          .end((err, res) => {
+            res.should.have.status(200);
+            res.body.should.be.a("array");
+            done();
+          });
+      }).timeout(5000);
+      it("Should get an array of articles", (done) => {
+        chai
+          .request(app)
+          .get("/entries")
+          .end((err, res) => {
+            res.should.have.status(200);
+            res.body.should.be.a("array");
+            done();
+          });
+      }).timeout(5000);
+    });
+  after(() => {
+    console.log("pass this");
 
-after(() => {
-  app.listen().close(() => {
-    mongoose.connection.close();
+    app.listen().close(() => {
+      mongoose.connection.close();
+    });
+    process.exit();
   });
 });

4072e984a4ae502febf0834b885f38a2ac70023a
Phaisan1233 <42394385+Phaisan1233@users.noreply.github.com>
2020-06-16 06:17:34 +1200

    travis ci test

--- backend/test.js
+++
@@ -1,6 +0,0 @@
-const fs = require("fs");
-const bibtexParse = require("bibtex-parse");
-
-let tdd = bibtexParse.entries(fs.readFileSync("tdd_articles.bib", "utf8"));
-
-console.log(JSON.stringify(tdd));
--- test/test.js
+++ test/test.js
@@ -23,117 +23,189 @@ describe("Array", function () {
 });
 
 describe("API Routing and MongoDB Testing", () => {
-  let id;
-
   after(() => {
     app.listen().close(() => {
       mongoose.db.dropCollection("entries", (err, result) => {});
+      mongoose.db.dropCollection("moderators", (err, result) => {});
       mongoose.close();
     });
   });
-  describe("Add TDD Articles to database", () => {
-    it("Should add tdd artilce to DB ", (done) => {
-      chai
-        .request(app)
-        .post("/entries/addTDD")
-        .send(tdd_article)
-        .end((err, res) => {
-          res.should.have.status(200);
-          assert.equal(res.body, "Entry added!");
-          done();
-        });
-    }).timeout(5000);
 
-    it("Should get an array of articles", (done) => {
-      chai
-        .request(app)
-        .get("/entries")
-        .end((err, res) => {
-          res.should.have.status(200);
-          res.body.should.be.a("array");
-          assert.equal(res.body.length, 23);
-          id = res.body[0]._id;
-          done();
-        });
-    }).timeout(5000);
+  describe("Search article list", () => {
+    let id;
+    describe("Add TDD Articles to database", () => {
+      it("Should add tdd artilce to DB ", (done) => {
+        chai
+          .request(app)
+          .post("/entries/addTDD")
+          .send(tdd_article)
+          .end((err, res) => {
+            res.should.have.status(200);
+            assert.equal(res.body, "Entry added!");
+            done();
+          });
+      }).timeout(5000);
+
+      it("Should get an array of articles", (done) => {
+        chai
+          .request(app)
+          .get("/entries")
+          .end((err, res) => {
+            res.should.have.status(200);
+            res.body.should.be.a("array");
+            assert.equal(res.body.length, 23);
+            id = res.body[0]._id;
+            done();
+          });
+      }).timeout(5000);
+    });
+
+    describe("Add a article and get and delete", () => {
+      it("Should add a new article called test Article", (done) => {
+        const article = {
+          type: "article",
+          title: "test artcle",
+          author: "test me",
+          journal: "myself",
+          year: "2020",
+          month: "may",
+        };
+        chai
+          .request(app)
+          .post("/entries/add")
+          .send(article)
+          .end((err, res) => {
+            res.should.have.status(200);
+            assert.equal(res.body, "Entry added!");
+            done();
+          });
+      }).timeout(5000);
+
+      it("Should return a article called test article", (done) => {
+        chai
+          .request(app)
+          .get("/entries/id" + id)
+          .end((err, res) => {
+            res.should.have.status(200);
+            res.body.should.be.a.property("_id");
+            res.body.should.be.a.property("title");
+            res.body._id.should.equal(id);
+            done();
+          });
+      }).timeout(5000);
+
+      it("Should delete and return test article", (done) => {
+        chai
+          .request(app)
+          .delete("/entries/delete/id" + id)
+          .end((err, res) => {
+            res.should.have.status(200);
+            res.body.should.be.a.property("_id");
+            res.body.should.be.a.property("title");
+            res.body._id.should.equal(id);
+            done();
+          });
+      }).timeout(5000);
+    });
+
+    describe("Search articles on database", () => {
+      it("Should return array of title search result", (done) => {
+        chai
+          .request(app)
+          .post("/entries/search/title")
+          .send({ text: "from" })
+          .end((err, res) => {
+            res.should.have.status(200);
+            res.body.should.be.a("array");
+            assert.equal(res.body.length, 1);
+            done();
+          });
+      }).timeout(5000);
+
+      it("Should return array of author search result", (done) => {
+        chai
+          .request(app)
+          .post("/entries/search/author")
+          .send({ text: "new" })
+          .end((err, res) => {
+            res.should.have.status(200);
+            res.body.should.be.a("array");
+            assert.equal(res.body.length, 1);
+            done();
+          });
+      }).timeout(5000);
+    });
   });
 
-  describe("Add a article and get and delete", () => {
-    it("Should add a new article called test Article", (done) => {
-      const article = {
-        type: "article",
-        title: "test artcle",
-        author: "test me",
-        journal: "myself",
-        year: "2020",
-        month: "may",
-      };
-      chai
-        .request(app)
-        .post("/entries/add")
-        .send(article)
-        .end((err, res) => {
-          res.should.have.status(200);
-          assert.equal(res.body, "Entry added!");
-          done();
-        });
-    }).timeout(5000);
+  describe("Moderator article list", () => {
+    // router.get("/", moderatorController.moderator);
+    // router.route("/add").post(moderatorController.addModerator);
+    // router.route("/id:id").get(moderatorController.getModerator);
+    // router.route("/delete/id:id").delete(moderatorController.delete);
+    // router.route("/move").post(moderatorController.moveToAnalyst);
+    let id;
+    describe("ADD an article in moderator list", () => {
+      it("Should add a new article called moderator test Article on moderator list", (done) => {
+        const article = {
+          type: "article",
+          title: "moderator test artcle",
+          author: "test me",
+          journal: "myself",
+          year: "2020",
+          month: "may",
+        };
+        chai
+          .request(app)
+          .post("/moderator/add")
+          .send(article)
+          .end((err, res) => {
+            res.should.have.status(200);
+            assert.equal(res.body, "Entry added to Moderator Queue!");
+            done();
+          });
+      }).timeout(5000);
+    });
+    describe("Get all Articles in moderator list", () => {
+      it("Should get an array of articles", (done) => {
+        chai
+          .request(app)
+          .get("/moderator")
+          .end((err, res) => {
+            res.should.have.status(200);
+            res.body.should.be.a("array");
+            assert.equal(res.body.length, 1);
+            id = res.body[0]._id;
+            done();
+          });
+      }).timeout(5000);
+    });
 
-    it("Should return a article called test article", (done) => {
-      chai
-        .request(app)
-        .get("/entries/id" + id)
-        .end((err, res) => {
-          res.should.have.status(200);
-          res.body.should.be.a.property("_id");
-          res.body.should.be.a.property("title");
-          res.body._id.should.equal(id);
-          done();
-        });
-    }).timeout(5000);
+    describe("Add a article and get and delete to moderator list", () => {
+      it("Should return a article called test article on moderator list", (done) => {
+        chai
+          .request(app)
+          .get("/moderator/id" + id)
+          .end((err, res) => {
+            res.should.have.status(200);
+            res.body.should.be.a.property("_id");
+            res.body.should.be.a.property("title");
+            res.body._id.should.equal(id);
+            done();
+          });
+      }).timeout(5000);
 
-    it("Should delete and return test article", (done) => {
-      chai
-        .request(app)
-        .delete("/entries/delete/id" + id)
-        .end((err, res) => {
-          res.should.have.status(200);
-          res.body.should.be.a.property("_id");
-          res.body.should.be.a.property("title");
-          res.body._id.should.equal(id);
-          done();
-        });
-    }).timeout(5000);
+      it("Should delete and return test article", (done) => {
+        chai
+          .request(app)
+          .delete("/moderator/delete/id" + id)
+          .end((err, res) => {
+            res.should.have.status(200);
+            res.body.should.be.a.property("_id");
+            res.body.should.be.a.property("title");
+            res.body._id.should.equal(id);
+            done();
+          });
+      }).timeout(5000);
+    });
   });
-
-  describe("Search articles on database", () => {
-    it("Should return array of title search result", (done) => {
-      chai
-        .request(app)
-        .post("/entries/search/title")
-        .send({ text: "from" })
-        .end((err, res) => {
-          res.should.have.status(200);
-          res.body.should.be.a("array");
-          assert.equal(res.body.length, 1);
-          done();
-        });
-    }).timeout(5000);
-
-    it("Should return array of author search result", (done) => {
-      chai
-        .request(app)
-        .post("/entries/search/author")
-        .send({ text: "new" })
-        .end((err, res) => {
-          res.should.have.status(200);
-          res.body.should.be.a("array");
-          assert.equal(res.body.length, 1);
-          done();
-        });
-    }).timeout(5000);
-  });
-
-  // router.route("/search/title").post(entryController.searchTitle);
-  // router.route("/search/author").post(entryController.searchAuthor);
 });
